7dea08f8b6d23d6c41be70507e7c3939143188d8
Yaso007 <yashopmukhiazz007@gmail.com>
2023-09-13 16:54:34 +0530

    doope nigga get me outta here

---
+++ lab3a.java
@@ -0,0 +1,7 @@
+/*Write a program in java to implement  a class called student having ID,Name,Branch,gender,address, and methods fetdata and displydata.Use scanner class  to get data from the console. */
+
+class a{
+    public static void main(String[] args){
+        System.out.println("Hii");
+    }
+}
